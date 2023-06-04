822e18e7483982a7e2b9abf7849468e0d1ccbefe
Akshay Chincholkar <akshaychincholkar@gmail.com>
2023-06-04 23:08:50 +0530

    table short template creation

--- revision-manager/src/App.js
+++ revision-manager/src/App.js
@@ -2,14 +2,17 @@ function App(){
     return (
         <>
             <div>
-                <lable>Email ID:</lable>
-                <input  type="email" placeholder="email"></input>
-                <br></br>
-                <lable>Password</lable>
-                <input></input>
-                <br></br>
-                <button>Login</button>
-                <button>Cancel</button>
+                <form action="\components\Table.html">
+                    <lable>Email ID:</lable>
+                    <input  type="email" placeholder="email"></input>
+                    <br></br>
+                    <lable>Password</lable>
+                    <input></input>
+                    <br></br>
+                    {/* <input type="submit">Login</input>
+                    <input>Cancel</input> */}
+                    <input type="submit" name="Login"></input>
+                </form>
             </div>
         </>
     );
